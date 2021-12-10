64cf9be46c42c22551bac859c52cbf0085884d0b
priscila monteiro <pririmonteiro@me.com>
2021-12-09 21:04:23 -0800

    seed created

--- data/migrations/20211210043350_projects-table.js
+++ data/migrations/20211210043350_projects-table.js
@@ -1,8 +1,64 @@
-
+/* eslint-disable no-undef */
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable("projects", function(table) {
+      table.increments("project_id");
+      table.string("project_name", 128).notNullable();
+      table.text("project_description");
+      table.boolean("project_completed").defaultTo(false);
+    })
+
+    .createTable("resources", function(table) {
+      table.increments("resource_id");
+      table
+        .string("resource_name", 128)
+        .notNullable()
+        .unique()
+      table.text("resource_description");
+    })
+
+    .createTable("tasks", function(table) {
+      table.increments("task_id");
+      table.string("task_description", 128).notNullable();
+      table.text("task_notes");
+      table.boolean("task_completed").defaultTo(false);
+      table
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+
+    .createTable("project_resources", function(table) {
+      table
+        .increments("assignment_id");
+      table
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("resource_id")
+        .inTable("resources")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });   
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists("project_resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
 };
+
--- data/seeds/projects-seed.js
+++ data/seeds/projects-seed.js
@@ -1,13 +1,66 @@
+const projects = [
+  {
+    project_name: 'Web API',
+    project_description:
+      'Build APIs',
+  },
+  {
+    project_name: 'Databases',
+    project_description:
+      'Learn SQL',
+  },
+  {
+    project_name: 'Authentication',
+    project_description:
+      '',
+  },
+];
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+const resources = [
+  {
+    resource_name: 'keyboard',
+    resource_description: 'resource 1 - this is optional',
+  },
+  {
+    resource_name: 'Computer',
+    resource_description: 'Windows PC',
+  },
+];
+
+const tasks = [
+  {
+    project_id: 1,
+    task_description: 'Do foo',
+    task_notes: 'step 1',
+  },
+  {
+    project_id: 1,
+    task_description: 'Do bar',
+    task_notes: 'Use Postman!',
+  },
+  {
+    project_id: 1,
+    task_description: 'Do baz',
+    task_notes: 'Have fun!',
+  },
+];
+
+const project_resources = [
+ {
+    project_id: 1,
+    resource_id: 1,
+  },
+  {
+    project_id: 2,
+    resource_id: 2,
+  }, 
+];
+
+
+exports.seed = async function(knex) {
+  await knex('projects').insert(projects)
+  await knex('resources').insert(resources)
+  await knex('tasks').insert(tasks)
+  await knex('project_resources').insert(project_resources)
+}
+
